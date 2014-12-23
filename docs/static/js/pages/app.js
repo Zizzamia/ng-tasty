fb4d9f5a32565fa534f4720d134e2678b79153c4
Leonardo Zizzamia <lzizzamia@twitter.com>
2014-12-22 19:28:20 -0800

    Update version in docs.

--- docs/static/js/pages/app.js
+++ docs/static/js/pages/app.js
@@ -7,7 +7,7 @@ angular.module('myApp.pages.app', [])
       $window.ga('send', 'pageview', { page: '/ng-tasty' + $location.path() });
     });
   }
-  $rootScope.version = '0.4.1';
+  $rootScope.version = '0.4.2';
 
   $scope.showDownloadModal = function() {
     modalInstance = $modal.open({
